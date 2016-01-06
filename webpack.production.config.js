1118064925449df01e49096db80faba1a67d3759
Christine Cha <hello@christinecha.com>
2016-01-06 18:45:26 -0500

    dumb

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,9 +1,9 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, '/src/app.js'),
+    entry:( __dirname, '/src/app.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: (__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
